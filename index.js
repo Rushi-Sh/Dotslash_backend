021ce38650203559f2c203075556a31c93fbb551
Rushi Shah <rushi2706@gmail.com>
2024-09-12 20:43:15 +0530

    render inactivity solution test fail

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require("axios");
-
-const url = `https://urban-snap-app-modified-backend.onrender.com/`; 
-const interval = 30000; 
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-setInterval(reloadWebsite, interval);
 
 const app = express();
 
