6e45364e479fd525554e23b40ce36c567cf0f7af
Rushi Shah <rushi2706@gmail.com>
2024-10-13 00:18:59 +0530

    updated News 1

--- routes/us_usernews.js
+++ routes/us_usernews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {handleNewsUpload,handleNewsPost} = require("../controllers/us_usercomplain")
+const {handleNewsUpload,handleNewsPost} = require("../controllers/us_usernews")
 
 router.get('/', (req, res) => {
     res.send({ status: "News Section is running" });
